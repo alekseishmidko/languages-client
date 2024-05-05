18119211e5177fec4b75eeb0074d8ae701d6742e
alekseishmidko <118507792+alekseishmidko@users.noreply.github.com>
2024-05-05 23:37:17 +0300

    05.05.24-1

--- app/auth/page.tsx
+++
@@ -1,9 +0,0 @@
-import AuthPage from "./AuthPage";
-
-export default function Auth() {
-  return (
-    <>
-      <AuthPage />
-    </>
-  );
-}
--- app/components/Common/Header/Header.tsx
+++ app/components/Common/Header/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button, Icon, Theme } from "@gravity-ui/uikit";
+import { Button, Icon, Modal, Theme, Card, Text } from "@gravity-ui/uikit";
 import { Moon, Sun } from "@gravity-ui/icons";
 import Link from "next/link";
 import { APP_ROUTES } from "@/app/utils/constants/route.constants";
@@ -9,6 +9,9 @@ import { DARK, LIGHT, THEME } from "@/app/utils/constants/theme.constants";
 import { SquareBracketsLetterA } from "@gravity-ui/icons";
 import Cookies from "js-cookie";
 import LocaleSelector from "../LocaleSelector/LocaleSelector";
+import { useEarthoOne } from "@eartho/one-client-react";
+import Spinner from "../Spinner/Spinner";
+import { Person, Xmark, ArrowRightFromSquare } from "@gravity-ui/icons";
 export default function Header() {
   const { theme, handleTheme } = useStore();
   const onThemeHandle = () => {
@@ -17,7 +20,12 @@ export default function Header() {
 
     handleTheme();
   };
-
+  const [open, setOpen] = React.useState(false);
+  const { isLoading, isConnected, error, user, connectWithPopup, logout } =
+    useEarthoOne();
+  const handleLogout = () => {
+    logout().then(() => setOpen(false));
+  };
   return (
     <header
       className={`flex justify-between items-center p-2   sticky top-0  `}
@@ -51,17 +59,73 @@ export default function Header() {
         </Link>
       </div>
 
-      <div className="flex space-x-4 mr-2">
-        <Button size="l" view="normal-contrast" onClick={onThemeHandle}>
+      <div className="flex space-x-4 mr-1">
+        <button
+          onClick={onThemeHandle}
+          className={`w-8 flex items-center justify-center  `}
+        >
           <Icon data={theme === LIGHT ? Sun : Moon} />
-        </Button>
+        </button>
         <LocaleSelector />
-        <Link href={APP_ROUTES.AUTH}>
-          <Button size="l" pin="round-round" selected view="outlined-success">
-            Вход
-          </Button>
-        </Link>
+
+        <button
+          onClick={() => setOpen(true)}
+          className={`w-8 flex items-center justify-center  `}
+        >
+          {isLoading ? (
+            <Spinner size={12} />
+          ) : (
+            <div>
+              <Icon data={Person} />
+            </div>
+          )}
+        </button>
       </div>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div className="flex flex-col   gap-6 p-20  rounded-[16px] relative">
+          {isConnected ? (
+            <Text variant="display-2" className="mb-6">
+              <span>{user?.displayName}</span>
+            </Text>
+          ) : (
+            <Text variant="display-2" className="mb-6">
+              Войти в систему
+            </Text>
+          )}
+
+          {isConnected ? (
+            <Button view="outlined-info" size="xl" onClick={handleLogout}>
+              Выйти <Icon data={ArrowRightFromSquare} />
+            </Button>
+          ) : (
+            <Button
+              view="outlined-info"
+              size="xl"
+              onClick={() =>
+                connectWithPopup({
+                  accessId: `${process.env.NEXT_PUBLIC_ACCESS_ID}`,
+                  enabledAuthProviders: [
+                    "facebook",
+                    "google",
+                    "github",
+                    "yandex",
+                    "vk",
+                  ],
+                })
+              }
+            >
+              Войти
+            </Button>
+          )}
+
+          <div
+            onClick={() => setOpen(false)}
+            className="absolute right-2 top-2 p-1   cursor-pointer hover:rounded-full  "
+          >
+            <Icon data={Xmark} />
+          </div>
+        </div>
+      </Modal>
     </header>
   );
 }
